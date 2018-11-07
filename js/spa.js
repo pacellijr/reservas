87e7468e1cd940247e24b7464dfbd47ac81b0f1f
Eugenio Pacelli Jr <pacellijr@gmail.com>
2018-11-06 22:29:14 -0200

    alterando mecanismo de carga de pedacos da pagina, usando ajax

---
+++ js/spa.js
@@ -0,0 +1,13 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+
+    $ajaxUtils.write("main","main.html");
+    $ajaxUtils.write("navbar","menus/navbar.html");
+    $ajaxUtils.write("sidebar","menus/sidebar.html");
+      
+       
+  }
+);
+
+
